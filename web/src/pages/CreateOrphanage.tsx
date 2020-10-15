afb455f727bdc17c60e68d334814a2bd2ab616fa
Jmarcondes <marcondes166@gmail.com>
2020-10-15 19:43:55 -0300

    fourth day of NWL#3

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -85,7 +85,7 @@ export default function CreateOrphanage() {
             <legend>Dados</legend>
 
             <Map 
-              center={[-27.2092052,-49.6401092]} 
+              center={[-8.0631885,-34.8711892]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
               onclick={handleMapClick}
